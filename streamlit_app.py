8339f9cd29c59f0623f9a30ebf60792a6ffbd055
Vikashpandey98 <142513119+Vikashpandey98@users.noreply.github.com>
2023-11-10 14:36:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,12 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 st.dataframe(my_fruit_list)
 
 st.radio("Pick one", ["cats", "dogs"])
-st.camera_input("Take a picture")
+
 
 
 tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
 tab1.write("this is tab 1")
 tab2.write("this is tab 2")
+
+st.balloons()
+st.snow()

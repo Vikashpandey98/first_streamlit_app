cfee0b4e84218b10493322c391889cce6e040282
Vikashpandey98 <142513119+Vikashpandey98@users.noreply.github.com>
2023-11-15 10:47:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,41 @@
-import streamlit as st 
+import streamlit
 
-st.title("My Mom's New Healthy Dinner")
-st.header('Breakfast Menu')
-st.text('🥣 Omega 3 and Blueberry Oatmeal')
-st.text('🥗Kale, spinach and Rocket Smoothie')
-st.text('🐔Hard-Boiled Free-Range eggs')
-st.text('🥑🍞Avocado Toast')
+streamlit.title("My Mom's New Healthy Dinner")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗Kale, spinach and Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range eggs')
+streamlit.text('🥑🍞Avocado Toast')
 
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-st.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)
 
 
-st.header("Fruityvice Fruit Advice!")
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
  
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-st.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
-st.text(my_data_row)
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 
 
